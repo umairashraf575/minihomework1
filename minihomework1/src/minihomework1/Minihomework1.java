fdf12dd8d07ab1f4e9d05facb7aae72c12b3b2c8
umair ashraf <umair ashraf@Umair.station>
2023-10-01 21:27:52 +0100

    Third Commit Project Modified Successfully 2nd Attempt

--- minihomework1/src/minihomework1/Minihomework1.java
+++ minihomework1/src/minihomework1/Minihomework1.java
@@ -15,7 +15,7 @@ public class Minihomework1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Umair Ashraf 2022575");
+        System.out.println("Umair Ashraf 2022575 has completed his First Homework Succesfully");
     }
     
 }
